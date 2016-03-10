17b1a41a07d62a0beb63510e8ad0f38852133125
kevinbird61 <kevinbird61@gmail.com>
2016-03-10 21:30:29 +0800

    Update the Lab1.
    Add the play sound function in lab1.
    It can play the square wave.

--- Lab1/Lab1_Script.m
+++ Lab1/Lab1_Script.m
@@ -1,12 +1,14 @@
 % Calling Square_wave function 
-length = 1000;
-upper = 2;
-lower = 1;
-frequency = 1/100;
+length = 100000;
+upper = 1;
+lower = -1;
+frequency = 1/4000;
 Percent = 0.43;
-sampling_rate = 1/40;
+sampling_rate = 8000;
 
 [Wave , T] = Square_wave(upper,lower,frequency,Percent,length,sampling_rate);
 
+sound(Wave , sampling_rate);
+
 plot(T , Wave);
-axis([0 length -lower 2*upper]);
+axis([0 length 2*lower 2*upper]);
