c06111b0cab1b9975e6f3c9ed2104f3caf7aaf3a
kevinbird61 <kevinbird61@gmail.com>
2016-03-14 01:40:35 +0800

    Upload for the new Lab1.
    Modify the frequency and length conversion.
    Length : s -> ms
    so as change to frequency.

--- Lab1/Lab1_Script.m
+++ Lab1/Lab1_Script.m
@@ -1,14 +1,25 @@
 % Calling Square_wave function 
+% ==============================參數改動===============================
+% Length 100000 ms
 length = 100000;
-upper = 1;
-lower = -1;
-frequency = 1/4000;
-Percent = 0.43;
+% Setting Upper/Lower Bound 
+upper = 2;
+lower = -2;
+% Setting Period and Frequency
+% 週期長度我以Length的單位做計算，在這邊以一個ms來代表一個單位
+% 由於T陣列的index沒辦法使用小數點，所以用ms來表示更小的單位
+Period = 1/10; % 2000 ms
+frequency = 1/Period;
+% Setting the Percent, based on Duty cycle
+Duty_cycle = 860;
+Percent = Duty_cycle/Period;
 sampling_rate = 8000;
+% ==============================參數改動===============================
 
+% ==============================函式操作===============================
 [Wave , T] = Square_wave(upper,lower,frequency,Percent,length,sampling_rate);
-
+%撥出聲音
 sound(Wave , sampling_rate);
-
+%劃出圖形
 plot(T , Wave);
-axis([0 length 2*lower 2*upper]);
+axis([0 length/1000 2*lower 2*upper]);
--- Lab1/Square_wave.m
+++ Lab1/Square_wave.m
@@ -10,28 +10,40 @@ function [Wave , T] = Square_wave( upper , lower , F , Percent , length , Fs )
 %   ============== output ==============
 %   Wave : 每個點的值  (matrix)
 %   T : 每個點的時間點 (matrix)
-
+%   unit_conver : 單位轉換(時間單位: s -> ms)
+    unit_conver = 1000;
+    length = length*unit_conver;
     Wave = 1:length;
     T = 1:length;
-    Period = 1 / F; % get the period
+    %if F < 1
+        % 因為 F = 1/T 表示現在T使用者是以小數點來計
+    %    Period = (1 / F)*1000; % get the period
+    %else
+    %    Period = 1 / F; % get the period 
+    %end
+    Period = (1/F)*unit_conver;
     %Duty_cycle = (Percent * Period)/100;
     % =============== Check the frequency ===============
     if(F > (Fs/2))
        fprintf('Error Frequency ! Frequency must be smaller than Fs/2 !\n');
+       fprintf('Aliasing !\n');
        return;
     else
-       fprintf('Aliasing !\n');
+       fprintf('Frequency check is OK~ !\n');
     end
     % =============== Check the boundary ===============
     if(lower > upper)
        fprintf('Error ! Your Upper bound must greater than lower bound!\n'); 
        return;
+    else
+        % if upper > 1 and lower < -1
     end
     % ============== Start Plotting ==============
+    % Duty_point : 每個周期的開始點，從他開始計算每個wave的Duty cycle
     Duty_point = length / Period;
     for i = 1 : Duty_point
         for j = 1+(i-1)*Period : i*Period
-           %  
+           % point: Duty cycle的範圍
            point = (i-1)*Period + Period*(Percent);
            if(j < (point))
                Wave(j) = upper;
@@ -40,11 +52,9 @@ function [Wave , T] = Square_wave( upper , lower , F , Percent , length , Fs )
            end
         end
     end
-    
     % Plot the Figure (signal)
     return;
     %plot(X,Y);
     %axis([0 length -lower 2*upper]);
-    
 end
 
