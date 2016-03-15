3ba5826993c1b9a0264b6e1532a6c9cd0d83f38d
kevinbird61 <kevinbird61@gmail.com>
2016-03-15 13:44:29 +0800

    Update the ver.4 Square_Wave function.
    Modify the time interval and frequency problems.

--- Lab1/Lab1_Script.m
+++ Lab1/Lab1_Script.m
@@ -1,25 +1,15 @@
-% Calling Square_wave function 
-% ==============================參數改動===============================
-% Length 100000 ms
-length = 100000;
-% Setting Upper/Lower Bound 
-upper = 2;
-lower = -2;
-% Setting Period and Frequency
-% 週期長度我以Length的單位做計算，在這邊以一個ms來代表一個單位
-% 由於T陣列的index沒辦法使用小數點，所以用ms來表示更小的單位
-Period = 1/10; % 2000 ms
-frequency = 1/Period;
-% Setting the Percent, based on Duty cycle
-Duty_cycle = 860;
-Percent = Duty_cycle/Period;
-sampling_rate = 8000;
-% ==============================參數改動===============================
+% define variables
+upper = 1;
+lower = -1;
+F = 100;
+percent = 0.8;
+length = 1;
+Fs = 500;
 
-% ==============================函式操作===============================
-[Wave , T] = Square_wave(upper,lower,frequency,Percent,length,sampling_rate);
-%撥出聲音
-sound(Wave , sampling_rate);
-%劃出圖形
+
+[Wave , T] = Square_wave(upper , lower , F , percent , length , Fs);
+
 plot(T , Wave);
-axis([0 length/1000 2*lower 2*upper]);
+axis([0 length*F 2*lower 2*upper]);
+
+sound(Wave , 8000);
--- Lab1/Square_wave.m
+++ Lab1/Square_wave.m
@@ -1,60 +1,37 @@
-%function [Wave , T] = Square_wave( upper , lower , F ,Percent , length ,
-%Fs) => 輸出
-function [Wave , T] = Square_wave( upper , lower , F , Percent , length , Fs )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-%   ============== input ==============
-%   amplitude : upper / lower
-%   F : frequency => 1 / T ; length : 整段測資的範圍 ; Fs : sampling rate
-%   Percent : Duty cycle / Period(T) => 0~1
-%   ============== output ==============
-%   Wave : 每個點的值  (matrix)
-%   T : 每個點的時間點 (matrix)
-%   unit_conver : 單位轉換(時間單位: s -> ms)
-    unit_conver = 1000;
-    length = length*unit_conver;
-    Wave = 1:length;
-    T = 1:length;
-    %if F < 1
-        % 因為 F = 1/T 表示現在T使用者是以小數點來計
-    %    Period = (1 / F)*1000; % get the period
-    %else
-    %    Period = 1 / F; % get the period 
-    %end
-    Period = (1/F)*unit_conver;
-    %Duty_cycle = (Percent * Period)/100;
-    % =============== Check the frequency ===============
-    if(F > (Fs/2))
-       fprintf('Error Frequency ! Frequency must be smaller than Fs/2 !\n');
-       fprintf('Aliasing !\n');
-       return;
-    else
-       fprintf('Frequency check is OK~ !\n');
-    end
-    % =============== Check the boundary ===============
-    if(lower > upper)
-       fprintf('Error ! Your Upper bound must greater than lower bound!\n'); 
-       return;
-    else
-        % if upper > 1 and lower < -1
-    end
-    % ============== Start Plotting ==============
-    % Duty_point : 每個周期的開始點，從他開始計算每個wave的Duty cycle
-    Duty_point = length / Period;
-    for i = 1 : Duty_point
-        for j = 1+(i-1)*Period : i*Period
-           % point: Duty cycle的範圍
-           point = (i-1)*Period + Period*(Percent);
-           if(j < (point))
-               Wave(j) = upper;
-           else
-               Wave(j) = lower;               
-           end
-        end
-    end
-    % Plot the Figure (signal)
-    return;
-    %plot(X,Y);
-    %axis([0 length -lower 2*upper]);
+% Program based on "octave".
+function [Wave , T] = Square_wave(upper , lower , F , percent , length , Fs)
+ % Initializing
+ Period = 1/F;
+ size = length / Period;
+ % Return value Initializing
+ Wave = 1 : size*F;
+ T = 1 : size*F;
+ 
+ % check the frequency
+ if(F > (Fs/2))
+  fprintf('Error Frequency ! Frequency must be smaller than Fs/2!\n');
+  fprintf('Aliasing!\n'); 
+  return;
+ else
+  fprintf('Frequency is OK!');
+ end
+ % check the  boundary
+ if(lower > upper)
+  fprintf('Error ! Your upper bound must be greater than lower bound!\n');
+  return;
+ else
+  fprintf('Boundary check is fine!\n');
+ end
+ 
+ % Start calculating
+ for i = 1 : size*F
+  if(mod(i,size) <= size*percent)
+    Wave(i) = upper;
+  else
+    Wave(i) = lower;
+  end
+  T(i) = i*Period;
+ end
+ 
+ return;
 end
-
