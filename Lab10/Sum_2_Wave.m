1bff4b553b19faca847ba87a8b1c0cee7ecee399
kevinbird61 <kevinbird61@gmail.com>
2016-06-07 13:58:09 +0800

    Upload the correct version.

--- Lab10/Sum_2_Wave.m
+++
@@ -1,9 +0,0 @@
-function [ result ] = Sum_2_Wave( Wave1 , Wave2 , samplePoint )
-% For summation of 2 wave
-result = zeros(1 , samplePoint);
-for i = 1 : samplePoint
-   result(i) = Wave1(i) + Wave2(i);
-end
-    return;
-end
-
